2fca94bb25a412cbb1913ad4a6d91fb5177532c1
aloy <loyad01@gmail.com>
2023-07-12 15:46:20 -0500

    polishing histograms

--- R/plot.R
+++ R/plot.R
@@ -9,15 +9,21 @@
 #' @rdname plot
 #' @export 
 #' @method plot carlboot
-plot.carlboot <- function(x, bins = 30, ...) {
+plot.carlboot <- function(x, bins = 30, size = 5,...) {
   boot_stats <- as.numeric(x)
   ggplot2::ggplot(data = NULL, ggplot2::aes(x = boot_stats)) +
     ggplot2::geom_histogram(aes(y = after_stat(width*density)), bins = bins) +
-    ggplot2::scale_y_continuous(labels = scales::label_percent(), expand = expansion(mult = c(0, .05))) +
+    ggplot2::geom_point(aes(x = mean(x), y = 0, color = "Bootstrap", shape = "Bootstrap"), size = size) +
+    ggplot2::geom_point(aes(x = attr(x, "observed"), color = "Observed", shape = "Observed", y = 0), size = size) +
+    ggplot2::scale_y_continuous(labels = scales::label_percent(), expand = expansion(mult = c(0.05, .05))) +
     ggplot2::labs(
       title = "Bootstrap distribution",
       x = "statistics",
-      y = "Percentage"
+      y = "Percentage",
+      shape = "",
+      color = ""
     ) +
+    ggplot2::scale_shape_manual(values = c(16, 4)) + 
+    ggplot2::scale_color_manual(values = c("darkorange", "skyblue")) + 
     ggplot2::theme_classic()
 }
